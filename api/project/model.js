e05cd2a52b53abfc0769f04a7132d253b5cb9313
krsnyder <kirkrobertsnyder@gmail.com>
2021-04-16 12:53:06 -0400

    finished getProjects model function

--- api/project/model.js
+++ api/project/model.js
@@ -1,1 +1,24 @@
-// build your `Project` model here
+const db = require('../../data/dbConfig');
+
+// !!(value) returns the boolean
+async function getProjects() {
+  const rows = await db('projects');
+
+  const result = [];
+
+  rows.forEach(row => {
+    result.push({
+      project_id: row.project_id,
+      project_name: row.project_name,
+      project_description: row.project_description,
+      project_completed: !!row.project_completed,
+    });
+  });
+
+  return result;
+}
+
+module.exports = {
+  getProjects,
+
+};
--- api/project/router.js
+++ api/project/router.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
+const Projects = require('./model');
 
 router.get('/', (req, res) => {
-  res.status(200).json({
-    message: 'Received Projects',
-  });
+  Projects.getProjects()
+    .then(projects => {
+      res.status(200).json(projects);
+    });
 });
 
 router.post('/', (req, res) => {
