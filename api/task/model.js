4b535093e5773d8dab68ae6e77d62c1bd7a5d0da
krsnyder <kirkrobertsnyder@gmail.com>
2021-04-16 18:30:28 -0400

    BACK IN BUSINESS BABY CANT STOP WONT STOP GAMESTOP

--- api/task/model.js
+++ api/task/model.js
@@ -1,1 +1,32 @@
-// build your `Task` model here
+const db = require('../../data/dbConfig');
+
+async function getTasks() {
+  const rows = await db('tasks as t')
+    .leftJoin('projects as p', 'p.project_id', '=', 't.project_id')
+    .select(
+      't.task_id',
+      't.task_description',
+      't.task_notes',
+      't.task_completed',
+      'p.project_name',
+      'p.project_description',
+    );
+
+  rows.forEach(row => {
+    row.task_completed = !!row.task_completed;
+  });
+
+  return rows;
+}
+
+async function postTasks(newTasks) {
+  const newPost = await db('tasks').insert(newTasks)
+    .then(([id]) => db('tasks').where('tasks_id', '=', id).first());
+
+  return newPost;
+}
+
+module.exports = {
+  getTasks,
+  postTasks,
+};
--- api/task/router.js
+++ api/task/router.js
@@ -1,9 +1,12 @@
 const router = require('express').Router();
+const Tasks = require('./model');
 
-router.get('/', (req, res) => {
-  res.status(200).json({
-    message: 'Received Tasks',
-  });
+router.get('/', (req, res, next) => {
+  Tasks.getTasks()
+    .then(tasks => {
+      res.status(200).json(tasks);
+    })
+    .catch(next);
 });
 
 router.post('/', (req, res) => {
