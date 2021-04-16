fd110121285df42a9ec316adf77fbeb725c4f85c
krsnyder <kirkrobertsnyder@gmail.com>
2021-04-16 07:55:53 -0400

    set up router

--- api/server.js
+++ api/server.js
@@ -1,1 +1,18 @@
-// build your server here and require it from index.js
+const express = require('express');
+const projectsRouter = require('./project/router');
+
+const server = express();
+
+server.use(express.json());
+
+server.use('api/projects', projectsRouter);
+
+server.use('*', (err, req, res, next) => { // eslint-disable-line
+  res.status(500).json({
+    customMessage: "We're screwed boys!",
+    message: err.message,
+    stack: err.stack,
+  });
+});
+
+module.exports = server;
--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// start your server here
+require('dotenv').config();
+
+const server = require('./api/server');
+
+const { PORT } = process.env;
+
+server.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}...`);
+});
